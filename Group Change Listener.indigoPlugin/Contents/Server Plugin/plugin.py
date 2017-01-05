ed4f7ec22c8260e5b1be97dc709d0906f90d4fad
kmarkley <kmarkley@users.noreply.github.com>
2017-01-04 22:50:50 -0600

    first commit

---
+++ Group Change Listener.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -0,0 +1,148 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+####################
+# http://www.indigodomo.com
+
+import indigo
+
+# Note the "indigo" module is automatically imported and made available inside
+# our global name space by the host process.
+
+################################################################################
+class Plugin(indigo.PluginBase):
+    ########################################
+    def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
+        indigo.PluginBase.__init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs)
+    
+    ########################################
+    def __del__(self):
+        indigo.PluginBase.__del__(self)
+    
+    ########################################
+    # Start, Stop and Config changes
+    ########################################
+    def startup(self):
+        self.debug = self.pluginPrefs.get("showDebugInfo",False)
+        self.logger.debug(u"startup")
+        if self.debug:
+            self.logger.debug("Debug logging enabled")
+        self.triggersDict = dict()
+        indigo.devices.subscribeToChanges()
+        indigo.variables.subscribeToChanges()
+    
+    ########################################
+    def shutdown(self):
+        self.logger.debug("shutdown")
+    
+    ########################################
+    def closedPrefsConfigUi (self, valuesDict, userCancelled):
+        self.logger.debug("closedPrefsConfigUi")
+        if not userCancelled:
+            self.debug = valuesDict.get("showDebugInfo",False)
+            if self.debug:
+                self.logger.debug("Debug logging enabled")
+    
+    ########################################
+    def validateEventConfigUi(self, valuesDict, typeId, triggerId):
+        self.logger.debug("validateTriggerConfigUi")
+        errorsDict = indigo.Dict()
+        if valuesDict.get('saveBool',False):
+            if not valuesDict.get('saveVar',""):
+                errorsDict['saveVar'] = "Required when 'Save to variable?' checked"
+            elif valuesDict.get('saveVar') in valuesDict.get('triggerVariables',[]):
+                errorsDict['saveVar'] = "Can't save to a variable being monitored"
+        if len(errorsDict) > 0:
+            return (False, valuesDict, errorsDict)
+        return (True, valuesDict)
+    
+    ########################################
+    # Trigger handlers
+    ########################################
+    def triggerStartProcessing(self, trigger):
+        self.logger.debug("triggerStartProcessing: "+trigger.name)
+        theProps = trigger.pluginProps
+        if theProps.get('version','') != self.pluginVersion:
+            self.updateTriggerVersion(trigger)
+        if theProps.get('saveBool',False):
+            saveId = int(theProps.get('saveVar',0))
+        else:
+            saveId = 0
+        devList = []
+        for devId in theProps.get('triggerDevices',[]):
+            devList.append(int(devId))
+        varList = []
+        for varId in theProps.get('triggerVariables',[]):
+            varList.append(int(varId))
+        self.triggersDict[trigger.id] = {'devs':devList, 'vars':varList, 'save':saveId}
+    
+    ########################################    
+    def triggerStopProcessing(self, trigger):
+        self.logger.debug("triggerStopProcessing: "+trigger.name)
+        if trigger.id in self.triggersDict:
+            del self.triggersDict[trigger.id]
+    
+    ########################################    
+    def updateTriggerVersion(self, trigger):
+        theProps = trigger.pluginProps
+        theProps['version'] = self.pluginVersion
+        trigger.replacePluginPropsOnServer(theProps)
+    
+    ########################################
+    # Device or Variable updated
+    ########################################
+    def deviceUpdated(self, oldDev, newDev):
+        for tid in self.triggersDict:
+            if newDev.id in self.triggersDict[tid]['devs']:
+                if newDev.states != oldDev.states:
+                    self.logger.debug("deviceUpdated: "+newDev.name)
+                    self.saveTriggeringObject(tid, newDev.name)
+                    indigo.trigger.execute(tid)
+    
+    ########################################
+    def variableUpdated(self, oldVar, newVar):
+        for tid in self.triggersDict:
+            if newVar.id in self.triggersDict[tid]['vars']:
+                if newVar.value != oldVar.value:
+                    self.logger.debug("variableUpdated: "+newVar.name)
+                    self.saveTriggeringObject(tid, newVar.name)
+                    indigo.trigger.execute(tid)
+    
+    ########################################
+    def saveTriggeringObject(self, tid, name):
+        saveId = self.triggersDict[tid]['save']
+        if saveId:
+            try:
+                indigo.variable.updateValue(saveId, name)
+            except:
+                self.logger.error("Trigger could not be saved to Indigo variable id '%s'. Does it exist?" % saveId)
+    
+    ########################################    
+    # Menu methods
+    ########################################        
+    def logTriggers(self):
+        separator = "".rjust(50,'-')
+        justCount = 12
+        myTriggers = indigo.triggers.iter('self')
+        if not myTriggers:
+            self.logger.info("No triggers configured")
+            return
+        self.logger.info("")
+        self.logger.info("List all triggers:")
+        self.logger.info(separator)
+        for trigger in myTriggers:
+            theProps = trigger.pluginProps
+            self.logger.info("Trigger: ".rjust(justCount)+trigger.name+" ("+str(trigger.id)+")")
+            self.logger.info("Enabled: ".rjust(justCount)+str(trigger.enabled))
+            if theProps.get('saveBool',False):
+                var = indigo.variables[theProps.get('saveVar',"")]
+                self.logger.info("Save To: ".rjust(justCount)+var.name+" ("+str(var.id)+")")
+            prefix = "Devices: ".rjust(justCount)
+            for devId in theProps.get('triggerDevices',[]):
+                self.logger.info(prefix+indigo.devices[int(devId)].name+" ("+devId+")")
+                prefix = "".rjust(justCount)
+            prefix = "Variables: ".rjust(justCount)
+            for varId in theProps.get('triggerVariables',[]):
+                self.logger.info(prefix+indigo.variables[int(varId)].name+" ("+varId+")")
+                prefix = "".rjust(justCount)
+            self.logger.info(separator)
+        self.logger.info("")
