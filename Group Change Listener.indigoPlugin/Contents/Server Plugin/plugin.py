382d8726752da1bbcf1b830571bb8669b3fa655f
kmarkley <kirk@kirkmarkleydesign.com>
2019-04-06 13:49:31 -0500

    remove ghpu

--- Group Change Listener.indigoPlugin/Contents/Server Plugin/plugin.py
+++ Group Change Listener.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -5,7 +5,6 @@
 
 import indigo
 import time
-from ghpu import GitHubPluginUpdater
 
 # Note the "indigo" module is automatically imported and made available inside
 # our global name space by the host process.
@@ -24,14 +23,11 @@ defaultProps = {
     'saveVar'           : "",
     }
 
-k_updateCheckHours = 24
-
 ################################################################################
 class Plugin(indigo.PluginBase):
     #-------------------------------------------------------------------------------
     def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
         indigo.PluginBase.__init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs)
-        self.updater = GitHubPluginUpdater(self)
 
     #-------------------------------------------------------------------------------
     def __del__(self):
@@ -41,7 +37,6 @@ class Plugin(indigo.PluginBase):
     # Start, Stop and Config changes
     #-------------------------------------------------------------------------------
     def startup(self):
-        self.nextCheck = self.pluginPrefs.get('nextUpdateCheck',0)
         self.debug = self.pluginPrefs.get("showDebugInfo",False)
         self.logger.debug(u"startup")
         if self.debug:
@@ -53,7 +48,6 @@ class Plugin(indigo.PluginBase):
     #-------------------------------------------------------------------------------
     def shutdown(self):
         self.logger.debug("shutdown")
-        self.pluginPrefs['nextUpdateCheck'] = self.nextCheck
         self.pluginPrefs['showDebugInfo'] = self.debug
 
     #-------------------------------------------------------------------------------
@@ -63,17 +57,6 @@ class Plugin(indigo.PluginBase):
             self.debug = valuesDict.get("showDebugInfo",False)
             if self.debug:
                 self.logger.debug("Debug logging enabled")
-
-    #-------------------------------------------------------------------------------
-    def runConcurrentThread(self):
-        self.logger.debug("runConcurrentThread")
-        try:
-            while True:
-                if time.time() > self.nextCheck:
-                    self.checkForUpdates()
-                self.sleep(600)
-        except self.StopThread:
-            pass    # Optionally catch the StopThread exception and do any needed cleanup.
 
     #-------------------------------------------------------------------------------
     # Trigger Handlers
@@ -200,27 +183,6 @@ class Plugin(indigo.PluginBase):
 
             self.logger.info(separator)
         self.logger.info("")
-
-    #-------------------------------------------------------------------------------
-    def checkForUpdates(self):
-        try:
-            self.updater.checkForUpdate()
-        except Exception as e:
-            msg = 'Check for update error.  Next attempt in {} hours.'.format(k_updateCheckHours)
-            if self.debug:
-                self.logger.exception(msg)
-            else:
-                self.logger.error(msg)
-                self.logger.debug(e)
-        self.nextCheck = time.time() + k_updateCheckHours*60*60
-
-    #-------------------------------------------------------------------------------
-    def updatePlugin(self):
-        self.updater.update()
-
-    #-------------------------------------------------------------------------------
-    def forceUpdate(self):
-        self.updater.update(currentVersion='0.0.0')
 
     #-------------------------------------------------------------------------------
     def toggleDebug(self):
